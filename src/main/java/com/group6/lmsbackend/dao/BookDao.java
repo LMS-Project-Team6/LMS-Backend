203462de85a9815d89bd189e04f3f0ce45d912ce
dhcode2004 <20232953@g.deu.ac.kr>
2025-05-31 12:42:08 +0900

    도서 추가 기능

---
+++ src/main/java/com/group6/lmsbackend/dao/BookDao.java
@@ -0,0 +1,16 @@
+package com.group6.lmsbackend.dao;
+
+import com.group6.lmsbackend.mapper.BookMapper;
+import com.group6.lmsbackend.vo.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDao {
+
+    @Autowired
+    private BookMapper bookMapper;
+
+    public int addBook(Book book) { return bookMapper.addBook(book); }
+
+}
---
+++ src/main/java/com/group6/lmsbackend/vo/Book.java
@@ -0,0 +1,45 @@
+package com.group6.lmsbackend.vo;
+
+public class Book {
+    private String bookId;
+    private String bookTitle;
+    private String bookWriter;
+    private String bookPublisher;
+    private String bookCNum;
+    private int lendNY;
+    private String createdDate;
+    private String modifiedDate;
+
+    public String getBookTitle() { return bookTitle; }
+
+    public void setBookTitle(String bookTitle) { this.bookTitle = bookTitle; }
+
+    public String getBookId() { return bookId; }
+
+    public void setBookId(String bookId) { this.bookId = bookId; }
+
+    public String getBookWriter() { return bookWriter; }
+
+    public void setBookWriter(String bookWriter) { this.bookWriter = bookWriter; }
+
+    public String getBookPublisher() { return bookPublisher; }
+
+    public void setBookPublisher(String bookPublisher) { this.bookPublisher = bookPublisher; }
+
+    public String getBookCNum() { return bookCNum; }
+
+    public void setBookCNum(String bookCNum) { this.bookCNum = bookCNum; }
+
+    public String getCreatedDate() { return createdDate; }
+
+    public void setCreatedDate(String createdDate) { this.createdDate = createdDate; }
+
+    public String getModifiedDate() { return modifiedDate; }
+
+    public void setModifiedDate(String modifiedDate) { this.modifiedDate = modifiedDate; }
+
+    public int getLendNY() { return lendNY; }
+
+    public void setLendNY(int lendNY) { this.lendNY = lendNY; }
+
+}
